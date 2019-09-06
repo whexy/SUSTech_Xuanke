0321f1492110d0ea65a5016b02f7bef93808a19b
Whexy <whexy@outlook.com>
2019-09-06 13:43:45 +0800

    Update
    new function!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import time
 import sys
 import json
+import getpass
 classList = {}
 s = requests.Session()
 
@@ -35,7 +36,7 @@ def rush_group(classList):
 
 def rush(num):
     operlist = ["bxqjhxkOper", "fawxkOper", "knjxkOper", "ggxxkxkOper"]
-    url = "http://jwxt.sustech.edu.cn/jsxsd/xsxkkc/"
+    url = "https://jwxt.sustech.edu.cn/jsxsd/xsxkkc/"
     urllist = [url+i+"?jx0404id="+num+"&xkzy=&trjf=" for i in operlist]
     for urls in urllist:
         r = s.get(urls)
@@ -48,35 +49,42 @@ def rush(num):
 
 
 if __name__ == '__main__':
+    username = input("id:")
+    password = getpass.getpass()
     with open("course.json", "r") as f:
         init_data = f.read()
         classList = json.loads(init_data)
     r = s.get(
-        'http://cas.sustech.edu.cn/cas/login?service=http%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL')
+        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL')
     data = {
-        'username': '',  # Here inputs your StudentID
-        'password': '',  # Here inputs your password
+        'username': username,
+        'password': password,
         'execution': re.findall('on" value="(.+?)"', r.text)[0],
         '_eventId': 'submit',
         'geolocation': ''
     }
 
     r = s.post(
-        'http://cas.sustech.edu.cn/cas/login?service=http%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL', data)
+        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL', data)
     print(r)
+    if str(r) != "<Response [200]>":
+        print("CAS 验证失败，请重新运行脚本。按回车键结束。")
+        input()
+        exit(-1)
     print("CAS验证成功")
-    print("教务系统启动")
     print("等待选课开始")
     while True:
         r = s.get(
-            'http://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL')
+            'https://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL')
         key = re.findall('href="(.+)" target="blank">进入选课', r.text)
         if len(key) > 0:
             break
         time.sleep(random.random()*random.random())
 
     k = key[0]
-    s.get('http://jwxt.sustech.edu.cn' + k)
+    s.get('https://jwxt.sustech.edu.cn' + k)
     print("------抢课开始------")
     rush_manager()
     s.close()
+    print("脚本运行结束，按回车键退出。")
+    input()
