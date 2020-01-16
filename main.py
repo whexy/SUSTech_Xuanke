fd28fdfdcac0e26669c741f43333d3048ba59cc9
Whexy <whexy@outlook.com>
2020-01-16 16:42:54 +0800

    Update
    For 2020 Spring

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import re
 import time
 import sys
 import json
-import getpass
-classList = {}
+courseList = {}
 s = requests.Session()
 
 
 def rush_manager():
-    global classList
-    while len(classList.keys()) != 0:
-        unhappyList = {}
-        for group in classList:
-            if rush_group(classList[group]):
-                print("当前组别选课成功")
+    global courseList
+    while len(courseList.keys()) != 0:
+        failedList = {}
+        for group in courseList:
+            if rush_group(courseList[group]):
+                print(group + "组选课成功")
             else:
-                unhappyList[group] = classList[group]
-        classList = unhappyList
+                failedList[group] = courseList[group]
+        courseList = failedList
         print("开始下一轮。")
 
 
-def rush_group(classList):
-    for aSimpleClass in classList:
-        course_id = classList[aSimpleClass]
+def rush_group(courseList):
+    for aSimpleClass in courseList:
+        course_id = courseList[aSimpleClass]
         if rush(course_id):
             print("已抢到", aSimpleClass)
             return True
@@ -37,12 +36,12 @@ def rush_group(classList):
 def rush(num):
     operlist = ["bxqjhxkOper", "fawxkOper", "knjxkOper", "ggxxkxkOper"]
     url = "https://jwxt.sustech.edu.cn/jsxsd/xsxkkc/"
-    urllist = [url+i+"?jx0404id="+num+"&xkzy=&trjf=" for i in operlist]
+    urllist = [url + i + "?jx0404id=" + num + "&xkzy=&trjf=" for i in operlist]
     for urls in urllist:
         r = s.get(urls)
         result = str(r.text)
         print(result)
-        time.sleep(random.random()*random.random())
+        time.sleep(random.random() * random.random())
         if result.find("true") >= 1:
             return True
     return False
@@ -50,12 +49,9 @@ def rush(num):
 
 if __name__ == '__main__':
     username = input("id:")
-    password = getpass.getpass()
+    password = input("pwd:")
     with open("course.json", "r") as f:
-        init_data = f.read()
-        classList = json.loads(init_data)
-    r = s.get(
-        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL')
+        courseList = json.loads(f.read())
     data = {
         'username': username,
         'password': password,
@@ -63,9 +59,9 @@ if __name__ == '__main__':
         '_eventId': 'submit',
         'geolocation': ''
     }
-
     r = s.post(
-        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL', data)
+        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL',
+        data)
     print(r)
     if str(r) != "<Response [200]>":
         print("CAS 验证失败，请重新运行脚本。按回车键结束。")
@@ -75,11 +71,12 @@ if __name__ == '__main__':
     print("等待选课开始")
     while True:
         r = s.get(
-            'https://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL')
+            'https://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL'
+        )
         key = re.findall('href="(.+)" target="blank">进入选课', r.text)
         if len(key) > 0:
             break
-        time.sleep(random.random()*random.random())
+        time.sleep(random.random() * random.random())
 
     k = key[0]
     s.get('https://jwxt.sustech.edu.cn' + k)
