8de77a3d30a5730821e4a7641015a4591f31986b
whexy <whexy@outlook.com>
2020-09-06 10:18:51 +0800

    2020-2021-1 update

--- main.py
+++ main.py
@@ -1,87 +1,90 @@
+import asyncio
+import datetime
 import random
-import requests
 import re
 import time
-import sys
-import json
-courseList = {}
-s = requests.Session()
+
+import aiohttp
+
+OPER_list = ["bxqjhxkOper", "fawxkOper", "knjxkOper", "ggxxkxkOper"]
+
+XK_URL = "https://jwxt.sustech.edu.cn/jsxsd/xsxkkc/"
+XK_Center_URL = 'https://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL'
+CAS_URL = "https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL"
+ROOT = 'http://jwxt.sustech.edu.cn'
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 '
+                  'Safari/537.36',
+    'Connection': 'keep-alive',
+    'Referer': 'http://jwxt.sustech.edu.cn/'
+}
 
 
-def rush_manager():
-    global courseList
-    while len(courseList.keys()) != 0:
-        failedList = {}
-        for group in courseList:
-            if rush_group(courseList[group]):
-                print(group + "组选课成功")
-            else:
-                failedList[group] = courseList[group]
-        courseList = failedList
-        print("开始下一轮。")
+async def handle(username, password, courseList):
+    async with aiohttp.ClientSession(headers=headers) as session:
+        login_status = await cas_login(session, username, password)
+        if login_status != 200:
+            print(login_status)
+            print("Login Failed. Please check your username and password.")
+            return
+        log("Login Success")
+        log("Waiting for beginning")
+        system_url = ROOT + await get_system_url(session)
+        async with session.get(system_url) as resp:
+            print(resp.cookies)
+        log("Begin!")
+        task = task_generator(session, courseList)
+        await asyncio.wait(task)
 
 
-def rush_group(courseList):
-    for aSimpleClass in courseList:
-        course_id = courseList[aSimpleClass]
-        if rush(course_id):
-            print("已抢到", aSimpleClass)
-            return True
-        else:
-            print("未抢到", aSimpleClass)
-    print("本组全部失败。准备进入下一组。")
-    return False
+def log(message):
+    print(str(datetime.datetime.now()), message)
 
 
-def rush(num):
-    operlist = ["bxqjhxkOper", "fawxkOper", "knjxkOper", "ggxxkxkOper"]
-    url = "https://jwxt.sustech.edu.cn/jsxsd/xsxkkc/"
-    urllist = [url + i + "?jx0404id=" + num + "&xkzy=&trjf=" for i in operlist]
-    for urls in urllist:
-        r = s.get(urls)
-        result = str(r.text)
-        print(result)
-        time.sleep(random.random() * random.random())
-        if result.find("true") >= 1:
-            return True
-    return False
+def task_generator(session, courseList):
+    task = []
+    for course_id in courseList:
+        url_list = [XK_URL + oper + "?jx0404id=" +
+                    course_id + "&xkzy=&trjf=" for oper in OPER_list]
+        for url in url_list:
+            future = asyncio.create_task(rush(session, url))
+            future.add_done_callback(lambda out: print(out.result()))
+            task.append(future)
+    return task
+
+
+async def rush(session, url):
+    async with session.get(url) as resp:
+        html_resp = await resp.text()
+        # return str(html_resp).find("true") != -1
+        return str(html_resp)
+
+
+async def get_system_url(session):
+    while True:
+        async with session.get(XK_Center_URL) as resp:
+            html_resp = await resp.text()
+            key = re.findall('href="(.+)" target="blank">进入选课', html_resp)
+            if len(key) > 0:
+                return key[0]
+        time.sleep(random.random())
+
+
+async def cas_login(session, username, password):
+    async with session.get(CAS_URL) as resp:
+        html_resp = await resp.text()
+        execution = re.findall('on" value="(.+?)"', html_resp)[0]
+    post_data = dict(username=username, password=password,
+                     execution=execution, _eventId='submit', geolocation='')
+    async with session.post(CAS_URL, data=post_data) as resp:
+        return resp.status
 
 
 if __name__ == '__main__':
-    username = input("id:")
-    password = input("pwd:")
-    with open("course.json", "r") as f:
-        courseList = json.loads(f.read())
-    data = {
-        'username': username,
-        'password': password,
-        'execution': re.findall('on" value="(.+?)"', r.text)[0],
-        '_eventId': 'submit',
-        'geolocation': ''
-    }
-    r = s.post(
-        'https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sustech.edu.cn%2Fjsxsd%2Fxsxk%2Fxklc_list%3FVes632DSdyV%3DNEW_XSD_PYGL',
-        data)
-    print(r)
-    if str(r) != "<Response [200]>":
-        print("CAS 验证失败，请重新运行脚本。按回车键结束。")
-        input()
-        exit(-1)
-    print("CAS验证成功")
-    print("等待选课开始")
-    while True:
-        r = s.get(
-            'https://jwxt.sustech.edu.cn/jsxsd/xsxk/xklc_list?Ves632DSdyV=NEW_XSD_PYGL'
-        )
-        key = re.findall('href="(.+)" target="blank">进入选课', r.text)
-        if len(key) > 0:
-            break
-        time.sleep(random.random() * random.random())
-
-    k = key[0]
-    s.get('https://jwxt.sustech.edu.cn' + k)
-    print("------抢课开始------")
-    rush_manager()
-    s.close()
-    print("脚本运行结束，按回车键退出。")
-    input()
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(
+        handle("11810000", "PA55W0RD", ["202020211000934", "202020211000933"]))
